ac8a8620fa5909c05cbd30d68fd7b4ce45684003
Ghosts <memories109@gmail.com>
2021-12-20 14:07:23 +0900

    basic

--- src/App.tsx
+++ src/App.tsx
@@ -19,10 +19,7 @@ export const App = () => (
       <Grid minH="100vh" p={3}>
         <ColorModeSwitcher justifySelf="flex-end" />
         <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
-          <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-          </Text>
+          
           
           <LoginForm />
         </VStack>
