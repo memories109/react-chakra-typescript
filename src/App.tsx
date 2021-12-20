79e37ee6678679cb9883b3167696df7529ef0afb
Ghosts <memories109@gmail.com>
2021-12-20 10:56:18 +0900

    react chakra typescript add

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import {
 } from "@chakra-ui/react"
 import { ColorModeSwitcher } from "./ColorModeSwitcher"
 import { Logo } from "./Logo"
+import LoginForm from './pages/LoginForm';
 
 export const App = () => (
   <ChakraProvider theme={theme}>
@@ -22,15 +23,8 @@ export const App = () => (
           <Text>
             Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
           </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Chakra
-          </Link>
+          
+          <LoginForm />
         </VStack>
       </Grid>
     </Box>
